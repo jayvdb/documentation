e16745f85a069864d3a258a472fd7121857b97a5
John Vandenberg <jayvdb@gmail.com>
2017-11-22 08:06:54 +0700

    Add sphinx-argparse~=0.2.1

--- conf.py
+++ conf.py
@@ -33,6 +33,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinxarg.ext',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
